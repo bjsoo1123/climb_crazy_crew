81c359e2032628823966c81b151d01d71dbf7390
jungsoo <bjsoo1123@dongguk.edu>
2024-06-03 08:13:40 +0900

    temp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,6 +70,12 @@ export default function Home() {
         //   return <>{html}</>;
         // }}
       />
+      <button
+        type="button"
+        className="rounded-full bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        일정 등록
+      </button>
     </main>
   );
 }
